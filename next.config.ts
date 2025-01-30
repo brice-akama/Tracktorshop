c2db8d0ab423b5623d21c86f75766168d10ada5b
Brice Ekuka <briceakama7@gmail.com>
2025-01-30 21:16:16 +0100

    Add ESLint ignoreDuringBuilds configuration

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com'], // Add Cloudinary's domain
   },
-  
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+  },
 };
 
 export default nextConfig;
